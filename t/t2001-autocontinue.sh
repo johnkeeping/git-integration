722ae6941e9381587d26ec2b4b1f885209f1612f
John Keeping <john@keeping.me.uk>
2013-04-09 22:44:20 +0100

    Add --autocontinue option
    This allows the integration to continue without stopping if the user has
    the rerere mechanism enabled and it successfully resolves all conflicts.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>

---
+++ t/t2001-autocontinue.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+test_description='Test autocontinue'
+. ./setup.sh
+
+test_expect_success 'setup branches' '
+	commit_file file base &&
+	git checkout -b branch1 &&
+	commit_file file branch1 &&
+	git checkout -b branch2 master &&
+	commit_file file branch2
+'
+
+test_expect_success 'create integration branch' '
+	test_config rerere.enabled true &&
+	test_must_fail git integration --create --add branch1 --add branch2 --rebuild pu &&
+	echo merged >file &&
+	git add file &&
+	git integration --continue
+'
+
+test_expect_success 'rebuild with --autocontinue' '
+	test_config rerere.enabled true &&
+	git integration --rebuild --autocontinue &&
+	git merge-base --is-ancestor branch1 HEAD &&
+	git merge-base --is-ancestor branch2 HEAD
+'
+
+test_expect_success 'rebuild with autocontinue in config' '
+	test_config rerere.enabled true &&
+	test_config integration.autocontinue true &&
+	git integration --rebuild &&
+	git merge-base --is-ancestor branch1 HEAD &&
+	git merge-base --is-ancestor branch2 HEAD
+'
+
+test_expect_success 'rebuild with autocontinue in config and --no-autocontinue' '
+	test_config rerere.enabled true &&
+	test_config integration.autocontinue true &&
+	test_when_finished git integration --abort &&
+	test_must_fail git integration --rebuild --no-autocontinue
+'
+
+test_done
