c3d08616acd756e71d02139f30e12e121c4dce0b
John Keeping <john@keeping.me.uk>
2013-12-15 11:33:44 +0000

    t/setup.sh: import test_line_count from git.git
    This provides better output when the line count is wrong.  It also
    handles excess whitespace around the output of "wc", for example on Mac
    OS X which adds leading whitespace before the line count.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>

--- t/setup.sh
+++ t/setup.sh
@@ -51,3 +51,26 @@ test_config () {
 	test_when_finished git config --unset "$@" &&
 	git config "$@"
 }
+
+# test_line_count checks that a file has the number of lines it
+# ought to. For example:
+#
+#	test_expect_success 'produce exactly one line of output' '
+#		do something >output &&
+#		test_line_count = 1 output
+#	'
+#
+# is like "test $(wc -l <output) = 1" except that it passes the
+# output through when the number of lines is wrong.
+
+test_line_count () {
+	if test $# != 3
+	then
+		error "bug in the test script: not 3 parameters to test_line_count"
+	elif ! test $(wc -l <"$3") "$1" "$2"
+	then
+		echo "test_line_count: line count for $3 !$1 $2"
+		cat "$3"
+		return 1
+	fi
+}
