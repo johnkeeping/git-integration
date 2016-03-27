4bb6e8a969eac4a71c60918edefe64947d1064ca
John Keeping <john@keeping.me.uk>
2016-03-27 14:17:22 +0100

    Preserve autocontinue state after stopping
    If a rebuild is started with --[no-]autocontinue, then after the first
    merge failure the state reverts to whatever is specified in the git
    config.  Save the autocontinue state and restore it during
    "git integration --continue" so that the value given with --rebuild is
    used for the entire integration run.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>

--- t/t2001-autocontinue.sh
+++ t/t2001-autocontinue.sh
@@ -8,13 +8,18 @@ test_expect_success 'setup branches' '
 	git checkout -b branch1 &&
 	commit_file file branch1 &&
 	git checkout -b branch2 master &&
-	commit_file file branch2
+	commit_file file branch2 &&
+	git checkout -b branch3 master &&
+	commit_file file branch3
 '
 
 test_expect_success 'create integration branch' '
 	test_config rerere.enabled true &&
-	test_must_fail git integration --create --add branch1 --add branch2 --rebuild pu &&
+	test_must_fail git integration --create --add branch1 --add branch2 --add branch3 --rebuild pu &&
 	echo merged >file &&
+	git add file &&
+	test_must_fail git integration --continue &&
+	echo merged-again >file &&
 	git add file &&
 	git integration --continue
 '
@@ -23,7 +28,8 @@ test_expect_success 'rebuild with --autocontinue' '
 	test_config rerere.enabled true &&
 	git integration --rebuild --autocontinue &&
 	git merge-base --is-ancestor branch1 HEAD &&
-	git merge-base --is-ancestor branch2 HEAD
+	git merge-base --is-ancestor branch2 HEAD &&
+	git merge-base --is-ancestor branch3 HEAD
 '
 
 test_expect_success 'rebuild with autocontinue in config' '
@@ -31,14 +37,17 @@ test_expect_success 'rebuild with autocontinue in config' '
 	test_config integration.autocontinue true &&
 	git integration --rebuild &&
 	git merge-base --is-ancestor branch1 HEAD &&
-	git merge-base --is-ancestor branch2 HEAD
+	git merge-base --is-ancestor branch2 HEAD &&
+	git merge-base --is-ancestor branch3 HEAD
 '
 
 test_expect_success 'rebuild with autocontinue in config and --no-autocontinue' '
 	test_config rerere.enabled true &&
 	test_config integration.autocontinue true &&
 	test_when_finished git integration --abort &&
-	test_must_fail git integration --rebuild --no-autocontinue
+	test_must_fail git integration --rebuild --no-autocontinue &&
+	git add file &&
+	test_must_fail git integration --continue
 '
 
 test_done
